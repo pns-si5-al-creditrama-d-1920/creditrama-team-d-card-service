345d79629eda9175940efb00e098f3181ef8786e
Nathan Strobbe <nathan.strobbe@etu.unice.fr>
2020-02-03 21:27:30 +0100

    Init spring boot project (#32)

---
+++ src/main/java/fr/unice/polytech/si5/al/creditrama/teamd/cardservice/CardServiceApplication.java
@@ -0,0 +1,17 @@
+package fr.unice.polytech.si5.al.creditrama.teamd.cardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableResourceServer
+@SpringBootApplication
+@EnableConfigurationProperties
+public class CardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardServiceApplication.class, args);
+    }
+
+}
